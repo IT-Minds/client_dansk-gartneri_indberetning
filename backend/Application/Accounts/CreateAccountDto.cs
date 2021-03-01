1f6adda11eea1d3efc9565938ed350e0fb9d39fb
Nicolai Wulff <ntw@it-minds.dk>
2021-03-01 14:50:06 +0100

    Remove automap from CreateAccountDto

--- backend/Application/Accounts/CreateAccountDto.cs
+++ backend/Application/Accounts/CreateAccountDto.cs
@@ -6,7 +6,7 @@ using Domain.Entities;
 
 namespace Application.Accounts
 {
-  public class CreateAccountDto : IAutoMap<Account>
+  public class CreateAccountDto
   {
     public string Email { get; set; }
     public string Password { get; set; }

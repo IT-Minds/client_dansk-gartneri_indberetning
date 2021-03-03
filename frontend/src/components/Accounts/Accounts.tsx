75317b2c121197238164ad2657d854411fe17c81
Nicolai Wulff <ntw@it-minds.dk>
2021-03-03 10:25:33 +0100

    Add handlers to all inputs of new account form. Now it can add a new account. Still needs more validation

--- frontend/src/components/Accounts/Accounts.tsx
+++ frontend/src/components/Accounts/Accounts.tsx
@@ -22,7 +22,7 @@ const Accounts: FC = () => {
   const fetchData = useCallback(async () => {
     try {
       const accountClient = await genAccountClient();
-      const data = await accountClient.getAllClients();
+      const data = await accountClient.getAllAccounts();
 
       console.log(data);
 

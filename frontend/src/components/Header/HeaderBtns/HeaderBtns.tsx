64c4a1d590fc6ebd9eb052b457f31e97a15d3a01
Nicolai Wulff <ntw@it-minds.dk>
2021-03-04 11:10:25 +0100

    Remove file obj/Infrastructure.EntityFrameworkCore.targets

---
+++ frontend/src/components/Header/HeaderBtns/HeaderBtns.tsx
@@ -0,0 +1,16 @@
+import { HStack } from "@chakra-ui/react";
+import { FC } from "react";
+
+import ColorModeBtn from "./ColorModeBtn";
+import LocaleBtn from "./LocaleBtn";
+import UserBtn from "./UserBtn";
+
+const HeaderButtons: FC = () => {
+  return (
+    <HStack spacing={3}>
+      <ColorModeBtn />
+      <UserBtn />
+    </HStack>
+  );
+};
+export default HeaderButtons;

2ffc4345ade1976a073e4e86d991362fbd62ea88
Nicolai Wulff <ntw@it-minds.dk>
2021-03-04 15:39:03 +0100

    Create query to get the current account

--- frontend/src/services/backend/nswagts.ts
+++ frontend/src/services/backend/nswagts.ts
@@ -128,6 +128,7 @@ export class ClientBase {
 export interface IAccountClient {
     createAccount(command: CreateAccountCommand): Promise<number>;
     getAllAccounts(): Promise<AccountDto[]>;
+    getAccount(): Promise<AccountDto>;
 }
 
 export class AccountClient extends ClientBase implements IAccountClient {
@@ -219,6 +220,42 @@ export class AccountClient extends ClientBase implements IAccountClient {
             });
         }
         return Promise.resolve<AccountDto[]>(<any>null);
+    }
+
+    getAccount(): Promise<AccountDto> {
+        let url_ = this.baseUrl + "/api/Account/myAccount";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        return this.transformOptions(options_).then(transformedOptions_ => {
+            return this.http.fetch(url_, transformedOptions_);
+        }).then((_response: Response) => {
+            return this.transformResult(url_, _response, (_response: Response) => this.processGetAccount(_response));
+        });
+    }
+
+    protected processGetAccount(response: Response): Promise<AccountDto> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = AccountDto.fromJS(resultData200);
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<AccountDto>(<any>null);
     }
 }
 

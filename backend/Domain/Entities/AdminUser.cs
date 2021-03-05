7c5462f24b3ca0520d2e420deb93693874a7937c
Nicolai Wulff <ntw@it-minds.dk>
2021-03-05 10:15:09 +0100

    Create admin entity

---
+++ backend/Domain/Entities/AdminUser.cs
@@ -0,0 +1,20 @@
+using System;
+using Domain.Common;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+  public class AdminUser : AuditableEntity, IUser
+  {
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public RoleEnum Role
+    {
+      get => RoleEnum.Admin;
+      set { throw new NotSupportedException("Tried to set the role of an admin."); }
+    }
+    public string Name { get; set; }
+    public DateTimeOffset? DeactivationTime { get; set; }
+  }
+}

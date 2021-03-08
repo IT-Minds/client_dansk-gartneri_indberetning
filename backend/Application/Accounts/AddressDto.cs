0dbc72ab8d10d1ecfc8ac444973b551e7a48a24b
Nicolai Wulff <ntw@it-minds.dk>
2021-03-08 11:27:49 +0100

    Add changes from feature/DGI-23 and update unittests and  dto's to reflect changes

--- backend/Application/Accounts/AddressDto.cs
+++ backend/Application/Accounts/AddressDto.cs
@@ -6,11 +6,10 @@ namespace Application.Accounts
 {
   public class AddressDto : IAutoMap<Address>
   {
-    public string StreetName { get; set; }
-    public string StreetNumber { get; set; }
-    public string PostCode { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string AddressLine3 { get; set; }
+    public string AddressLine4 { get; set; }
     public void Mapping(Profile profile)
     {
       profile.CreateMap<Address, AddressDto>();

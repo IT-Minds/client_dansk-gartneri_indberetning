b7bf5e2e2ed454583e00c46b4c84870434cb14e5
Nicolai Wulff <ntw@it-minds.dk>
2021-03-12 14:01:45 +0100

    Reset useAuth to previous commit after wrong merge

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -45,17 +45,23 @@ export const useAuth = (): AuthHook<IUserDto> => {
     console.log(loginRequest);
     const client = await genAuthenticationClient();
 
-      const user: string = await client.login().catch(() => null);
+    const user: IUserTokenDto = await client
+      .login(
+        new LoginCommand({
+          loginDetails: loginRequest
+        })
+      )
+      .catch(() => null);
 
-      if (!user) {
-        return false;
-      }
-      setAuthStage(AuthStage.CHECKING);
-      setCookie(user);
-      setAuthToken(user);
-      setAuthCounter(c => c + 1);
-      return true;
-    }, []);
+    if (!user) {
+      return false;
+    }
+    setAuthStage(AuthStage.CHECKING);
+    setCookie(user.token);
+    setAuthToken(user.token);
+    setAuthCounter(c => c + 1);
+    return true;
+  }, []);
 
   const logout = useCallback(() => {
     setAuthStage(AuthStage.CHECKING);

37a52af5db441b8bd7f529d6b9f567db3e659179
Nicolai Wulff <ntw@it-minds.dk>
2021-03-15 15:18:12 +0100

    Create CreateAccountantCommand and validator. First commit of branch feature/DGI-44

---
+++ backend/Application/Users/Commands/CreateAccountantCommand/CreateAccountantCommandValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+
+namespace Application.Users.Commands.CreateAccountantCommand
+{
+  public class CreateAccountantCommandValidator : AbstractValidator<CreateAccountantCommand>
+  {
+    public CreateAccountantCommandValidator()
+    {
+      RuleFor(e => e.AccountantDto)
+        .NotNull();
+      RuleFor(e => e.AccountantDto.AccountId)
+        .NotEmpty();
+      RuleFor(e => e.AccountantDto.Email)
+        .NotEmpty()
+        .EmailAddress();
+      RuleFor(e => e.AccountantDto.Name)
+        .NotEmpty()
+        .MaximumLength(200);
+    }
+  }
+}
---
+++ backend/Application/Users/CreateAccountantDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Users
+{
+  public class CreateAccountantDto
+  {
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int AccountId { get; set; }
+  }
+}

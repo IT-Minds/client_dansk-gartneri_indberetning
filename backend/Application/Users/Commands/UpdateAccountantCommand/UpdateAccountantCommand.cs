6a822ed28ef0101522f75d642a6f8f72cd17e1d6
Nicolai Wulff <ntw@it-minds.dk>
2021-03-16 11:30:06 +0100

    Start implementation of UpdateAccountantCommand. First commit of branch feature/DGI-45

---
+++ backend/Application/Users/Commands/UpdateAccountantCommand/UpdateAccountantCommand.cs
@@ -0,0 +1,54 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.ExampleChildren;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Application.Users.Commands.UpdateAccountantCommand
+{
+  public class UpdateAccountantCommand : IRequest
+  {
+    [JsonIgnore]
+    public int Id { get; set; }
+    public UserAccountIdDto Accountant { get; set; }
+
+
+    public class UpdateAccountantCommandHandler : IRequestHandler<UpdateAccountantCommand>
+    {
+      private readonly IApplicationDbContext _context;
+
+      public UpdateAccountantCommandHandler(IApplicationDbContext context)
+      {
+        _context = context;
+      }
+
+      public async Task<Unit> Handle(UpdateAccountantCommand request, CancellationToken cancellationToken)
+      {
+        var accountantEntity = await _context.Users.FindAsync(request.Id);
+
+        if (accountantEntity == null)
+        {
+          throw new NotFoundException(nameof(User), request.Id);
+        }
+
+        if (!await _context.ExampleParents.AnyAsync(e => e.Id == request.Accountant.ParentId, cancellationToken))
+        {
+          throw new NotFoundException(nameof(ExampleParent), request.Child.ParentId);
+        }
+
+        exampleEntity.Name = request.Child.Name;
+        exampleEntity.Type = request.Child.Type;
+        exampleEntity.ParentId = request.Child.ParentId;
+
+        _context.ExampleChildren.Update(exampleEntity);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+      }
+    }
+  }
+}

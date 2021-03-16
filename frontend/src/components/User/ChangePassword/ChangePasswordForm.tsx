f03d1c8a59b6efe7717e1516f53970619cdf5bcb
Nicolai Wulff <ntw@it-minds.dk>
2021-03-16 15:20:16 +0100

    Modify ChangePasswordForm to reflect recent changes to command

--- frontend/src/components/User/ChangePassword/ChangePasswordForm.tsx
+++ frontend/src/components/User/ChangePassword/ChangePasswordForm.tsx
@@ -4,7 +4,7 @@ import { useLocales } from "hooks/useLocales";
 import { usePasswordValidation } from "hooks/usePasswordValidation";
 import { FC, useCallback } from "react";
 import { genUserClient } from "services/backend/apiClients";
-import { UpdatePasswordCommand, UpdatePasswordDto } from "services/backend/nswagts";
+import { UpdatePasswordCommand } from "services/backend/nswagts";
 
 interface Props {
   onSubmit?: (e: React.FormEvent) => void;
@@ -23,9 +23,7 @@ const ChangePasswordForm: FC<Props> = ({ onSubmit }) => {
       await userClient.updatePassword(
         1, //TODO: Change to id of active user.
         new UpdatePasswordCommand({
-          passwordDto: new UpdatePasswordDto({
-            password: pwState.pw1
-          })
+          newPassword: pwState.pw1
         })
       );
       onSubmit(e);

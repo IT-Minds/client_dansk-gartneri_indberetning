1db1f83b95757635749d5de762c7ae9871c23c1f
Nicolai Wulff <ntw@it-minds.dk>
2021-03-17 10:52:22 +0100

    Create new Razor Class Libary project, RazorEmail, to manage email templates. Create first template.

---
+++ backend/RazorEmails/Views/Emails/ActivateUserEmail/ActivateUserEmailViewModel.cs
@@ -0,0 +1,11 @@
+namespace RazorEmails.Views.Emails.ActivateUserEmail
+{
+  public class ActivateUserEmailViewModel
+  {
+    public string Url { get; set; }
+    public ActivateUserEmailViewModel(string url)
+    {
+      Url = url;
+    }
+  }
+}

d834f2a32564b09722e1795f4293422708674255
Nicolai Wulff <ntw@it-minds.dk>
2021-03-19 10:36:11 +0100

    Add migration after change to User entity

---
+++ backend/Infrastructure/Persistence/Migrations/20210319093530_UserSSOToken.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class UserSSOToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SSOTokenId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SSOTokenId",
+                table: "Users");
+        }
+    }
+}
